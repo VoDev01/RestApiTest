48134d16053de1be119ee11076ff8fa963ca33d2
VoDev01 <vodev1405@gmail.com>
2023-10-03 16:12:29 +0300

    Tests

--- database/migrations/2023_10_03_124411_item_migration.php
+++ database/migrations/2023_10_03_124411_item_migration.php
@@ -1,18 +1,20 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
+class ItemMigration extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('item', function(Blueprint $table){
-            $table->id();
+            $table->increments('id');
             $table->char('name', 255)->nullable();
             $table->char('phone', 15)->nullable();
             $table->char('key', 25);
@@ -23,8 +25,8 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('item');
     }
-};
+}
--- tests/Feature/ItemTest.php
+++ tests/Feature/ItemTest.php
@@ -2,20 +2,87 @@
 
 namespace Tests\Feature;
 
+use App\Item;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ItemTest extends TestCase
 {
+    use DatabaseTransactions;
+
+    protected $item;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->item = factory(Item::class)->create();
+    }
+
     /**
-     * A basic test example.
+     * @group items
+     * Test all items show.
      *
      * @return void
      */
     public function testAll()
     {
-        $response = $this->get('/api/v1/items');
+        $response = $this->get('api/v1/items');
         $response->assertStatus(200);
+    }
+    /**
+     * @group items
+     * Test item show.
+     *
+     * @return void
+     */
+    public function testShow()
+    {
+        $response = $this->get('api/v1/items/show/1');
+        $response->assertStatus(200);
+    }
+    /**
+     * @group items
+     * Test item create.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $response = $this->post('api/v1/items/create', [
+            'name' => $this->item->name,
+            'phone' => $this->item->phone,
+            'key' => $this->item->key
+        ]);
+        $this->assertDatabaseHas('items', [
+            'name' => $this->item->name,
+            'phone' => $this->item->phone,
+            'key' => $this->item->key
+        ]);
+    }
+    /**
+     * @group items
+     * Test item update.
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $response = $this->post('api/v1/items/update', [
+            'id' => 1,
+            'name' => 'Das das'
+        ]);
+        $this->assertDatabaseHas('items', ['name' => 'Das das']);
+    }
+    /**
+     * @group items
+     * Test item delete.
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $response = $this->post('api/v1/items/delete/1');
+        $this->assertDatabaseMissing('items', ['id' => 1]);
     }
 }
